b84c43df7527eff330e9f00a37c913dc0a8c34fd
Nilej24 <nmanalo543@gmail.com>
2023-06-29 03:25:15 +0100

    added keyboard on save screen

---
+++ Assets/SaveManagerScript.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SaveManagerScript : MonoBehaviour
+{
+    private int currentChar = 1;
+    public TextMeshProUGUI char1, char2, char3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // input letters with the keyboard buttons
+    public void InputLetter(string letter) {
+
+        // cancel function if name is complete
+        if (currentChar > 3) return;
+
+        // update current onscreen letter
+        switch (currentChar) {
+
+            case 1:
+                char1.text = letter;
+                break;
+            case 2:
+                char2.text = letter;
+                break;
+            case 3:
+                char3.text = letter;
+                break;
+
+        }
+
+        // move to next letter
+        currentChar++;
+
+    }
+
+    // delete the previous letter
+    public void InputBackspace() {
+
+        // cancel function if name is empty
+        if (currentChar <= 1) return;
+
+        // delete prev onscreen letter
+        switch (currentChar) {
+
+            case 2:
+                char1.text = "_";
+                break;
+            case 3:
+                char2.text = "_";
+                break;
+            case 4:
+                char3.text = "_";
+                break;
+
+        }
+
+        // move to prev letter
+        currentChar--;
+        
+    }
+
+}
